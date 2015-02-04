af524c37f78edfc44343b618a1032d5f9bd3b3b4
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-02-04 18:21:23 +0900

    Use ENV

--- config/setting.rb
+++ config/setting.rb
@@ -2,14 +2,14 @@
 # For available variables, see
 # http://www.sinatrarb.com/configuration.html
 
-set :port, 4567
-set :mention_to, 'YourTwitterID'
+set :port, ENV['port']             || 4567
+set :mention_to, ENV['mention_to'] || 'YourTwitterID'
 set :allowed_ips, ['127.0.0.2', '207.97.227.253', '50.57.128.197', '108.171.174.178']
 
 # Twitter configuration
 Twitter.configure do |config|
-  config.consumer_key = 'consumer_key'
-  config.consumer_secret = 'consumer_secret'
-  config.oauth_token = 'your_oauth_token'
-  config.oauth_token_secret = 'oauth_token_secret'
+  config.consumer_key = ENV['twtr_consumer_key']        || 'consumer_key'
+  config.consumer_secret = ENV['consumer_secret']       || 'consumer_secret'
+  config.oauth_token = ENV['your_oauth_token']          || 'your_oauth_token'
+  config.oauth_token_secret = ENV['oauth_token_secret'] || 'oauth_token_secret'
 end
