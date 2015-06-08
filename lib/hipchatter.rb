a2f416ba22f0d3bf0d54708de48426d442cdb95c
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-06-08 18:15:50 +0900

    Support tag push

--- lib/hipchatter.rb
+++ lib/hipchatter.rb
@@ -4,11 +4,18 @@ class HipChatter
   def initialize(repository:, commits:, ref: 'refs/heads/master')
     @repository = repository
     @commits = commits
-    @branch = ref.split('refs/heads/', 2).last
+
+    @ref_name, @ref_path =
+      if ref.match('refs/heads/')
+        branch = ref.split('refs/heads/', 2).last
+        ["at #{branch}", "tree/#{branch}"]
+      elsif ref.match('refs/tags/')
+        tag = ref.split('refs/tags/', 2).last
+        ["tag #{tag}", "history/#{tag}"]
+      end
   end
 
   def notify(token:, to:, from: 'HipChatter')
-    return nil if @repository.nil? || @commits.nil? || @branch.nil?
     client = HipChat::Client.new(token)
     client[to].send(from, build_message, notify: true)
   end
@@ -16,11 +23,12 @@ class HipChatter
   private
 
   def build_message
-    message = "<a href='#{@repository['url']}'>#{@repository['name']}</a> (<a href='#{@repository['url']}/tree/#{@branch}'>#{@branch}</a>) is just updated"
+    message = "<a href='#{@repository['url']}'>#{@repository['name']}</a> (<a href='#{@repository['url']}/#{@ref_path}'>#{@ref_name}</a>) is just updated"
     message += commits_message
   end
 
   def commits_message
+    return '' if @commits.nil?
     limit = 4
     message = @commits[0...limit].reduce('') do |str, commit|
       str + "<br/>- #{commit['message'].split("\n").first} (<a href='#{commit['url']}'>#{commit['id'][0..7]}</a>)"
