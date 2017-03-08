632aac54d6b7081c0db76b866f03ddb7e7d2e9e8
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2017-03-08 13:31:56 +0900

    set slack_webhook_url

--- config/setting.rb
+++ config/setting.rb
@@ -2,4 +2,6 @@
 # For available variables, see
 # http://www.sinatrarb.com/configuration.html
 
-set :port, ENV['port']             || 4567
+set :port, ENV['PORT']             || 4567
+
+set :slack_webhook_url, ENV['SLACK_WEBHOOK_URL']
